66f61698cfcf0ff866cc16b37f784c332df4f9bd
Mohamed Anwar <76563640+MohamedAnwarAlhamed@users.noreply.github.com>
2023-03-16 21:01:39 +0300

    about section

--- js/main.js
+++ js/main.js
@@ -11,11 +11,11 @@ $(document).ready(function(){
         $('.fa-bars').removeClass('fa-times');
         $('.nav').removeClass('nav-toggle');
     
-        // if($(window).scrollTop() > 10){
-        //   $('header').addClass('header-active');
-        // }else{
-        //   $('header').removeClass('header-active');
-        // }
+        if($(window).scrollTop() > 10){
+          $('header').addClass('header-active');
+        }else{
+          $('header').removeClass('header-active');
+        }
     
       });
 });
